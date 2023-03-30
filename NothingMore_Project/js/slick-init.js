7ae284ba942f95863f2dc88c807ada8e8cd6f240
EgorArt17 <42829827+EgorArt17@users.noreply.github.com>
2023-03-30 23:11:07 +0300

    Init repo, added project
    siuuuuuu

---
+++ NothingMore_Project/js/slick-init.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+  $('.releases-slider').slick({
+  	infinite: true,
+  	slidesToShow: 4,
+  	slidesToScroll: 1,
+  	responsive: [
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        swipeToSlide: true
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        swipeToSlide: true
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        swipeToSlide: true
+      }
+    },
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        swipeToSlide: true
+      }
+    },
+    {
+      breakpoint: 1439,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        swipeToSlide: true
+      }
+    }
+  ]
+  });
+});
